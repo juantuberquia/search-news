b2d0b5fff496140dd2305907453a2f2e704a1dfd
juantuberquia <juantuberquia@users.noreply.github.com>
2020-10-07 15:47:55 -0500

    show news ok with styles

--- src/components/ShowNews.js
+++ src/components/ShowNews.js
@@ -1,13 +1,33 @@
 import React from "react";
+import styles from "./ShowNews.module.css";
 
 const ShowNews = ({ news }) => {
+  // col s12 m6 14
+
+  const [title, urlToImage, author, description, url] = news;
+
   return (
-    <div>
+    <div className={`${styles.container_main} `}>
       {news.map((article) => (
-        <div key={article.title}>
-          <p>{article.author}</p>
-          <p> {article.content}</p>
-          <p> {article.description}</p>
+        <div className={`${styles.container_card} card`} key={article.title}>
+          <div className={`${styles.img} card-image`}>
+            <img src={article.urlToImage} alt="title" />
+          </div>
+          <div className="card-content">
+            <h3>{article.title}</h3>
+            <p>{article.author}</p>
+            <p> {article.description}</p>
+          </div>
+          <div className="card-action">
+            <a
+              href={article.url}
+              target="_blank"
+              className="waves-effect waves-light btn"
+              rel="noopener noreferrer"
+            >
+              Ver mas
+            </a>
+          </div>
         </div>
       ))}
     </div>
